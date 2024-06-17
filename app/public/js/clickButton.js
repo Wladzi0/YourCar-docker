bd0deadec2b3dedc12f791bfb4db2ea92dbb1172
Wladzi0 <vladyslav.svizinskyi@dsnet.pl>
2024-06-17 18:30:44 +0200

    Feat: initialize symfony project

--- app/public/js/clickButton.js
+++
@@ -1,9 +0,0 @@
-$('#btnAdd').click(function(){
-    let url = window.location.href;
-      $.ajax({method: 'POST',
-        url: '/session/add',
-         data: {
-            'url': url
-         }
-      })
-});
